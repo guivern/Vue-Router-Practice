8d1272c912df80d6a6f642c1e49a631ef96d692e
Guillermo Verón <guiver95@gmail.com>
2022-06-20 17:39:06 -0400

    passing pagination through query params

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,67 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous
+      </router-link>
+      <router-link
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 
 export default {
   name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      default: 1
+    }
+  },
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    // cuando un componente se llama a si mismo vue lo reutiliza, por esto, el metodo created solo se ejecuta la primera vez
+    // watch effect permite que se ejecute el createad cada vez que page es modificado mediante reactividad
+    // la otra forma para volver a ejecutar el created cuando se llama al mismo componente es: <router-view :key="$route.fullPath" />
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then(response => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      const lastPage = Math.ceil(this.totalEvents / 2)
+      return this.page < lastPage
+    }
   }
 }
 </script>
@@ -36,5 +72,23 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
